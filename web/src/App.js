0eaae609bd7b44f9153dcb3f1040e465feb4e1e0
Oweta Jacob Emmy <oweta001@gmail.com>
2025-04-08 01:02:04 +0300

    Moved LoginScreen and SignupScreen to screens folder

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import SignupScreen from "./components/SignupScreen";
-import LoginScreen from "./components/LoginScreen";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   return (
-    <div>
-      <h1>HealthMobile</h1>
-      <SignupScreen />
-      <LoginScreen />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/signup" element={<SignupScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
